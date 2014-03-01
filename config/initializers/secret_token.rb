ed8f4670bbc9db787bf70d07b19b4f70a7baff57
Waqas Mubarik <mwaqas0031@gmail.com>
2014-03-01 22:14:22 +0500

    ac

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WaqasLight::Application.config.secret_key_base = 'a755dc01ce3576dffb888c7c5516c09a968eecdcdd115d9d237fcc4b42ad8158885e27d88f19d0bd5359c7078e1604f764cf2a026aab94a421d80d2908b3f5c8'
