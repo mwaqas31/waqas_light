8926cb15c2eeab1ded93360f2c62722235cc0ff1
Waqas Mubarik <mwaqas0031@gmail.com>
2014-03-22 16:05:33 +0500

    added parsley

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ WaqasLight::Application.routes.draw do
 
 root :to => "main#index"
 
+get "about-us",to: 'main#about_us' , as: 'about_us'
+get "contact-us",to: 'main#contact_us' , as: 'contact_us'
+get "pricing",to: 'main#pricing' , as: 'pricing'
+
+
+
 end
